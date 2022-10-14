1c51ff49c813859a6ead63c04d9cad74fde0d275
SharonNtuli <sharontuli315@gmail.com>
2022-10-14 15:03:13 +0200

    Initial commit

---
+++ Assets/Scripts/GroundPiece.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundPiece : MonoBehaviour
+{
+    public bool isColored = false;
+
+    public void Colored(Color color)
+    {
+        GetComponent<MeshRenderer>().material.color = color;
+        isColored = true;
+
+        //////FindObjectOfType<GameManager>().CheckComplete();
+        GameManager.singleton.CheckComplete();
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# roller-splat
+ level 6
